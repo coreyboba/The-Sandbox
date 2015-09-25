b3101677413f0398f13cc9d5c69b57af09143e10
Corey Boba <coreyboba@gmail.com>
2015-09-24 22:49:33 -0400

    Initial commit.

---
+++ The Sandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(The_Sandbox.Startup))]
+namespace The_Sandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
